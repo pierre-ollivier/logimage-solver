c3df42497223bcf8d9f8d7a650c0edd475fccd73
Pierre Ollivier <pierre.ollivier2001@gmail.com>
2022-11-04 20:42:44 +0100

    Add missing logimage attributes

--- logimage.py
+++ logimage.py
@@ -3,10 +3,12 @@ from typing import List
 
 class Logimage:
 
-    def __init__(self, leftConstraints: List[List[int]] = None, topConstraints: List[List[int]] = None):
-        if leftConstraints is None:
-            leftConstraints = []
-        if topConstraints is None:
-            topConstraints = []
-        self.height = len(leftConstraints)
-        self.width = len(topConstraints)
+    def __init__(self, left_constraints: List[List[int]] = None, top_constraints: List[List[int]] = None):
+        if left_constraints is None:
+            left_constraints = []
+        if top_constraints is None:
+            top_constraints = []
+        self.height = len(left_constraints)
+        self.width = len(top_constraints)
+        self.left_constraints = left_constraints
+        self.top_constraints = top_constraints
