92aeb741df8132f33211b1bfe6b8e5714a623093
Pierre Ollivier <pierre.ollivier2001@gmail.com>
2022-11-05 13:16:30 +0100

    Fix parenthesis

--- tests_ls/testutils.py
+++ tests_ls/testutils.py
@@ -1,16 +1,19 @@
 from utils import check_dim, currently_satisfied_constraints
 
+
 def test_checkdim():
     assert check_dim(constraints=[1, 2], board_extract=[
         1, 0, 0, 1, 1, 0, 0])
     assert check_dim(constraints=[1, 2], board_extract=[
         0, 1, 0, 0, 1, 1, 0, 0])
 
+
 def test_currently_satisfied_constraints():
-    assert currently_satisfied_constraints([1, 1, 1, 0, 1] == ([3, 1], True))
-    assert currently_satisfied_constraints([1, 1, 1, 0, 0] == ([3], False))
-    assert currently_satisfied_constraints([0, 0, 0, 0, 1] == ([1], True))
-    assert currently_satisfied_constraints([0, 0, 0, 0, 0] == ([], False))
+    assert currently_satisfied_constraints([1, 1, 1, 0, 1]) == ([3, 1], True)
+    assert currently_satisfied_constraints([1, 1, 1, 0, 0]) == ([3], False)
+    assert currently_satisfied_constraints([0, 0, 0, 0, 1]) == ([1], True)
+    assert currently_satisfied_constraints([0, 0, 0, 0, 0]) == ([], False)
+
 
 def run():
     test_checkdim()
