b0ebc5f527b916378a1974811c153233d9fc2460
Pierre Ollivier <pierre.ollivier2001@gmail.com>
2022-11-05 22:43:05 +0100

    Clean the test code - 0.07s for 5*8, 2.0s for 6*9

--- tests_ls/testutils.py
+++ tests_ls/testutils.py
@@ -3,19 +3,21 @@ import numpy as np
 
 
 def test_checkdim():
-    res = check_dim(constraints=[1, 2], board_extract=np.array([
+    assert check_dim(constraints=[1, 2], board_extract=np.array([
         1, 0, 0, 1, 1, 0, 0]))
-    print(res)
-    assert res
     assert check_dim(constraints=[1, 2], board_extract=np.array([
         0, 1, 0, 0, 1, 1, 0, 0]))
 
 
 def test_currently_satisfied_constraints():
-    assert currently_satisfied_constraints(np.array([1, 1, 1, 0, 1])) == ([3, 1], True)
-    assert currently_satisfied_constraints(np.array([1, 1, 1, 0, 0])) == ([3], False)
-    assert currently_satisfied_constraints(np.array([0, 0, 0, 0, 1])) == ([1], True)
-    assert currently_satisfied_constraints(np.array([0, 0, 0, 0, 0])) == ([], False)
+    assert currently_satisfied_constraints(
+        np.array([1, 1, 1, 0, 1])) == ([3, 1], True)
+    assert currently_satisfied_constraints(
+        np.array([1, 1, 1, 0, 0])) == ([3], False)
+    assert currently_satisfied_constraints(
+        np.array([0, 0, 0, 0, 1])) == ([1], True)
+    assert currently_satisfied_constraints(
+        np.array([0, 0, 0, 0, 0])) == ([], False)
 
 
 def run():
