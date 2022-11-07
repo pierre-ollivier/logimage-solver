5a5ce644ae07667a557e31ad1cf9391897e1a016
Pierre Ollivier <pierre.ollivier2001@gmail.com>
2022-11-07 22:54:46 +0100

    Add validation button and event when it is pressed

--- mainwindow.py
+++ mainwindow.py
@@ -3,4 +3,14 @@ import tkinter as tk
 main_window = tk.Tk()
 insert_label = tk.Label(text="Ajoutez votre image ici")
 insert_label.pack()
+
+
+def validation_button_pressed():
+    print("Button pressed")
+
+
+validation_button = tk.Button(
+    text="Valider", command=validation_button_pressed)
+validation_button.pack()
+
 main_window.mainloop()
