6c5ba5824c8d7df5903fbed30d69635e88e174e9
Pierre Ollivier <pierre.ollivier2001@gmail.com>
2022-11-08 23:14:51 +0100

    Correct issues with dimensions and flipping of the output image, with StringVars for text inputs

--- mainwindow.py
+++ mainwindow.py
@@ -7,7 +7,13 @@ from images import grayscale_img, resize_img, to_board
 
 imPath = "test"
 
-main_window = tk.Tk()
+LINES_COUNT = 3
+COLUMNS_COUNT = 3
+
+
+root = tk.Tk()
+svH = tk.StringVar()
+svV = tk.StringVar()
 insert_label = tk.Label(text="Ajoutez votre image ici")
 insert_label.pack()
 
@@ -28,30 +34,48 @@ def openImageFile():
     global img, image, imPath
     imPath = filedialog.askopenfilename(initialdir=".", title="Open an image", filetypes=(
         ("Image file", "*.png"), ("Image file", "*.jpeg"), ("Image file", "*.jpg"), ("All File Types", "*.*")))
-    print("Image successfully loaded")
+    # print("Image successfully loaded")
     if imPath:
         PILimg = Image.open(imPath)
         PILimg = PILimg.resize((200, 200))
         img = ImageTk.PhotoImage(PILimg)
         image.configure(image=img)
 
+
 def draw_board_in_canvas(canvas: tk.Canvas, board: Board) -> None:
+    global LINES_COUNT, COLUMNS_COUNT
+    draw_lines()
     data = board.data
-    #board.draw()
+    board.draw()
+    print(data.shape)
     for i, j in np.ndindex(data.shape):
-        if data[i][j] == 1:
-            canvas.create_rectangle(i * 200/6, j * 200/6, (i + 1) * 200/6, (j + 1) * 200/6, fill="#000")
+        if data[i, j] == 1:
+            canvas.create_rectangle(
+                i * 200/LINES_COUNT, j * 200/COLUMNS_COUNT, (i + 1) * 200/LINES_COUNT, (j + 1) * 200/COLUMNS_COUNT, fill="#000")
+            # i * 200/COLUMNS_COUNT, j * 200/LINES_COUNT, (i + 1) * 200/COLUMNS_COUNT, (j + 1) * 200/LINES_COUNT, fill="#000")
         else:
-            canvas.create_rectangle(i * 200/6, j * 200/6, (i + 1) * 200/6, (j + 1) * 200/6, fill="#fff")
+            canvas.create_rectangle(
+                i * 200/LINES_COUNT, j * 200/COLUMNS_COUNT, (i + 1) * 200/LINES_COUNT, (j + 1) * 200/COLUMNS_COUNT, fill="#fff")
+            # i * 200/COLUMNS_COUNT, j * 200/LINES_COUNT, (i + 1) * 200/COLUMNS_COUNT, (j + 1) * 200/LINES_COUNT, fill="#fff")
+
 
 def board_from_image(path: str):
-    global slider
+    global slider, LINES_COUNT, COLUMNS_COUNT
     img = grayscale_img(path)
-    img = resize_img(img, (6, 6)) #TODO
-    arr = np.array(img)
+    img = resize_img(img, (LINES_COUNT, COLUMNS_COUNT))  # TODO
+    arr = np.array(img).T
     board = to_board(arr, 255 - slider.get())
     return board
 
+
+def set_entry_horizontal():
+    global LINES_COUNT, svH
+    LINES_COUNT = int(svH.get())
+
+
+def set_entry_vertical():
+    global COLUMNS_COUNT, svV
+    COLUMNS_COUNT = int(svV.get())
 
 
 load_image_button = tk.Button(
@@ -59,6 +83,21 @@ load_image_button = tk.Button(
 load_image_button.pack()
 
 image.pack()
+
+label_entry_horizontal = tk.Label(
+    text="Entrez le nombre de lignes du logimage")
+entry_horizontal = tk.Entry(root,
+                            textvariable=svH, validate="focusout", validatecommand=set_entry_horizontal)
+label_entry_vertical = tk.Label(
+    text="Entrez le nombre de colonnes du logimage")
+entry_vertical = tk.Entry(root,
+                          textvariable=svV, validate="focusout", validatecommand=set_entry_vertical)
+
+label_entry_horizontal.pack()
+entry_horizontal.pack()
+label_entry_vertical.pack()
+entry_vertical.pack()
+
 
 slider = tk.Scale(from_=0, to=255, tickinterval=32,
                   length=250, orient="horizontal")
@@ -71,13 +110,17 @@ validation_button.pack()
 canvas_logimage = tk.Canvas(width=202, height=202)
 canvas_logimage.pack()
 
-LINES_COUNT = 6
-COLUMNS_COUNT = 6
-for line_number in range(LINES_COUNT + 1):
-    print(line_number)
-    print((0, line_number * 200/LINES_COUNT, 200, line_number * 200/LINES_COUNT))
-    canvas_logimage.create_line(0, line_number * 200/LINES_COUNT, 200, line_number * 200/LINES_COUNT)
-for column_number in range(COLUMNS_COUNT + 1):
-    canvas_logimage.create_line(column_number * 200/COLUMNS_COUNT, 0, column_number * 200/COLUMNS_COUNT, 200)
 
-main_window.mainloop()
+def draw_lines():
+    for line_number in range(LINES_COUNT + 1):
+        """print(line_number)
+        print((0, line_number * 200/LINES_COUNT,
+              200, line_number * 200/LINES_COUNT))
+        canvas_logimage.create_line(
+            0, line_number * 200/LINES_COUNT, 200, line_number * 200/LINES_COUNT)"""
+    for column_number in range(COLUMNS_COUNT + 1):
+        canvas_logimage.create_line(
+            column_number * 200/COLUMNS_COUNT, 0, column_number * 200/COLUMNS_COUNT, 200)
+
+
+root.mainloop()
